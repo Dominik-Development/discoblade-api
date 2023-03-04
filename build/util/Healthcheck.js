966f5060d1703b4d0157d99f5d8f6b3bed0fd65c
Dominik K <dominik@koch-bautechnik.de>
2023-03-04 02:33:22 +0100

    Delete build directory

--- build/util/Healthcheck.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Healthcheck(router) {
-    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
-}
-exports.default = Healthcheck;
